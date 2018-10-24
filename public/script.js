e1f1011591336f4c02d22a0422fa7ab8b13eee72
Aymeric De Abreu <ada@appouest.com>
2018-10-24 15:55:20 +0200

    Adding comments

--- public/script.js
+++ public/script.js
@@ -2,21 +2,27 @@ $(function () {
     // if user is running mozilla then use it's built-in WebSocket
     window.WebSocket = window.WebSocket || window.MozWebSocket;
   
+    //Remplacez ici l'adresse IP par celle de votre raspberry ou par son nom
     var connection = new WebSocket('ws://127.0.0.1:3030');
   
+    //Appelé lorsque la websocket se connecte correctement
     connection.onopen = function () {
         $('#status').text('Connecté');
     };
 
+    //Appelé lorsque la websocket se ferme (quand on termine le serveur par exemple)
     connection.onclose = function () {
         $('#status').text('Déconnecté');
     };
   
+    //Appelé si une erreur survient
     connection.onerror = function (error) {
         $('#status').text('Erreur');
         $('#content').append('Une erreur est survenue');
     };
   
+
+    //Appelé lorsque le serveur envoie un message
     connection.onmessage = function (message) {
         $('#status').text('Connecté');
         //Ici on transforme la touche 'entrée' en tag HTML <br /> pour créer un retour à la ligne
